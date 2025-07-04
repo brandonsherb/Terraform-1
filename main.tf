36839ba63babc597fa466d57071641d5288f821d
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2025-07-04 11:53:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-044415bb13eee2391" 
   instance_type = "t2.micro"
 
   tags = {
