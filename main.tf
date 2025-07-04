52c6a400162e874c26f5eeef7df33287feb9fe12
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2025-07-04 11:42:14 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
